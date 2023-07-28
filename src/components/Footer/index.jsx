9192ef0da890d3611fbeca2e4fcb9fa33829344a
ai-steven <michaeljackson90828@gmail.com>
2023-07-28 15:46:07 +0530

    Footer Black

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -15,14 +15,11 @@ export default function Footer() {
         <Section className="container">
           <Section className="cs-height_100 cs-height_lg_70" />
           <Section className="cs-footer_logo">
-            <img src="/images/logo_footer.svg" alt="logo" className="cs-hide_dark" />
+            <img src="/images/logo_footer_black.svg" alt="logo" className="cs-hide_dark" height = "100" width="300"/>
             <img src="/images/logo_footer_wh.svg" alt="logo" className="cs-hide_white" />
           </Section>
           <Section className="cs-height_25 cs-height_lg_25" />
           <Section className="cs-social_btns cs-center">
-            <Link to="/" className="cs-center cs-primary_color cs-accent_bg_hover cs-light_bg cs-white_color_hover">
-              <Icon icon="fa6-brands:facebook-f" />
-            </Link>
             <Link to="/" className="cs-center cs-primary_color cs-accent_bg_hover cs-light_bg cs-white_color_hover">
               <Icon icon="fa6-brands:twitter" />            
             </Link>
