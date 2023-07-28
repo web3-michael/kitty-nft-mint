6080f96723fbbd3fc2657dfd6e44d6d5326d3eb7
ai-steven <michaeljackson90828@gmail.com>
2023-07-28 22:18:23 +0530

    Home

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -25,19 +25,13 @@ export default function Header() {
                 <Section className="cs-nav">
                   <ul className="cs-nav_list" style={{display:`${mobileToggle?'block':'none'}`}}>
                     <li className="menu-item-has-children"><ScrollLink to="hero" spy={true} smooth={true} offset={-80} duration={500} onClick={()=>setMobileToggle(false)}>Home</ScrollLink>
-                    <DropDown>
-                      <ul>
-                        <li><Link to="/">Default Home</Link></li>
-                        <li><Link to="/home2">Home Version-2</Link></li>
-                        <li><Link to="/home3">Home Version-3</Link></li>
-                      </ul>
-                    </DropDown>
+               
                     </li>
                     <li><ScrollLink to="about" spy={true} smooth={true} offset={-80} duration={500} onClick={()=>setMobileToggle(false)}>About</ScrollLink></li>
                     <li><ScrollLink to="roadmap" spy={true} smooth={true} offset={-80} duration={500} onClick={()=>setMobileToggle(false)}>Roadmap</ScrollLink></li>
                     <li><ScrollLink to="team" spy={true} smooth={true} offset={-80} duration={500} onClick={()=>setMobileToggle(false)}>Team</ScrollLink></li>
                     <li><ScrollLink to="faq" spy={true} smooth={true} offset={-80} duration={500} onClick={()=>setMobileToggle(false)}>FAQ</ScrollLink></li>
-                    <li><ScrollLink to="contact" spy={true} smooth={true} offset={-80} duration={500} onClick={()=>setMobileToggle(false)}>Contact</ScrollLink></li>
+                
                   </ul>
                   <span className={mobileToggle?"cs-munu_toggle cs-toggle_active":"cs-munu_toggle"} onClick={()=>setMobileToggle(!mobileToggle)}><span></span></span>
                 </Section>
